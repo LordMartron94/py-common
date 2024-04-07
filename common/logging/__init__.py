6f91f42ceef5a28693ab3111a5103dfb3ca87f19
Matthew van der Hoorn <md.registrations@protonmail.com>
2024-04-07 20:38:41 +0200

    Add necessary imports to main __init__

--- common/logging/__init__.py
+++ common/logging/__init__.py
@@ -1,7 +1,5 @@
 from .hoorn_logger import HoornLogger
-from .output.hoorn_log_output_interface import HoornLogOutputInterface
-from .output.file_hoorn_log_output import FileHoornLogOutput
-from .output.default_hoorn_log_output import DefaultHoornLogOutput
+from .output import *
 
 from .log_type import LogType
 from .hoorn_log import HoornLog
