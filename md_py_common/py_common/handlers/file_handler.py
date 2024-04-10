edb04dc76f07410eda7a70e61929362433072814
Matthew van der Hoorn <md.registrations@protonmail.com>
2024-04-10 16:49:01 +0200

    Added a data save method to the file handler

--- md_py_common/py_common/handlers/file_handler.py
+++ md_py_common/py_common/handlers/file_handler.py
@@ -6,42 +6,50 @@ from typing_extensions import deprecated
 
 
 class FileHandler:
-	def is_file_of_type(self, file: Union[Path, str], extension: str) -> bool:
-		return Path(file).is_file() and str(file).lower().endswith(extension)
+    def is_file_of_type(self, file: Union[Path, str], extension: str) -> bool:
+        return Path(file).is_file() and str(file).lower().endswith(extension)
 
-	@deprecated("Use is_file_of_type instead.")
-	def is_mp4_file(self, file: Union[Path, str]):
-		"""Check if the file has a .mp4 extension."""
-		return Path(file).is_file() and str(file).lower().endswith('.mp4')
+    @deprecated("Use is_file_of_type instead.")
+    def is_mp4_file(self, file: Union[Path, str]):
+        """Check if the file has a .mp4 extension."""
+        return Path(file).is_file() and str(file).lower().endswith('.mp4')
 
-	@deprecated("Use is_file_of_type instead.")
-	def is_ts_file(self, file: Union[Path, str]):
-		return Path(file).is_file() and str(file).lower().endswith('.ts')
+    @deprecated("Use is_file_of_type instead.")
+    def is_ts_file(self, file: Union[Path, str]):
+        return Path(file).is_file() and str(file).lower().endswith('.ts')
 
-	def get_file_modified_date(self, file_path: str) -> float:
-		"""Get the modification time of a file."""
-		return os.path.getmtime(file_path)
+    def get_file_modified_date(self, file_path: str) -> float:
+        """Get the modification time of a file."""
+        return os.path.getmtime(file_path)
 
-	def get_number_of_files_in_dir(self, directory: Path, extension: str = "*") -> int:
-		if not directory.is_dir():
-			raise ValueError("The provided path is not a valid directory.")
+    def get_number_of_files_in_dir(self, directory: Path, extension: str = "*") -> int:
+        if not directory.is_dir():
+            raise ValueError("The provided path is not a valid directory.")
 
-		# Count the number of files in the folder
-		if extension == "*":
-			num_files = sum(1 for _ in directory.iterdir() if _.is_file())
-		else:
-			num_files = sum(1 for _ in directory.iterdir() if self.is_file_of_type(_, extension))
+        # Count the number of files in the folder
+        if extension == "*":
+            num_files = sum(1 for _ in directory.iterdir() if _.is_file())
+        else:
+            num_files = sum(1 for _ in directory.iterdir() if self.is_file_of_type(_, extension))
 
-		return num_files
+        return num_files
 
-	def get_children_paths(self, directory: Path, extension: str) -> List[Path]:
-		if not directory.is_dir():
-			raise ValueError("The provided path is not a valid directory.")
-		
-		paths: List[Path] = []
+    def get_children_paths(self, directory: Path, extension: str) -> List[Path]:
+        if not directory.is_dir():
+            raise ValueError("The provided path is not a valid directory.")
 
-		for file_path in directory.iterdir():
-			if file_path.suffix == extension:
-				paths.append(file_path)
-		
-		return paths
+        paths: List[Path] = []
+
+        for file_path in directory.iterdir():
+            if file_path.suffix == extension:
+                paths.append(file_path)
+
+        return paths
+
+    def save_dict_to_file(self, data: dict, file_path: Path, header: str = None):
+        with open(file_path, 'w') as file:
+            if header is not None:
+                file.write(f"{header}\n\n")
+
+            for key, value in data.items():
+                file.write(f"{key}: {value}\n")
