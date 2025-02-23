bb258dfacd80524a1561e86aff522a32aa9a8743
Matthew van der Hoorn <md.registrations@protonmail.com>
2025-02-23 04:46:21 +0100

    Added functionality for an exit condition for pipeline
    Makes it easier to control the flow without making things chaotic (only a single pipe type has access to signaling this)

--- py_common/patterns/pipeline/abstract_pipeline.py
+++ py_common/patterns/pipeline/abstract_pipeline.py
@@ -1,21 +1,42 @@
 from abc import ABC, abstractmethod
-from typing import Any, List
+from typing import Any, List, Callable
 
 from .pipe import IPipe
+
+class ExitCheckPipe(IPipe):
+    def __init__(self, exit_condition: Callable[[Any], bool], signal_exit_func: Callable):
+        self._exit_condition = exit_condition
+        self._signal_exit_func = signal_exit_func
+
+    def flow(self, data: Any) -> Any:
+        if self._exit_condition(data): self._signal_exit_func()
 
 
 class AbPipeline(ABC):
     def __init__(self):
         self._pipes: List[IPipe] = []
+        self._data: Any = None
+        self._must_exit: bool = False
+
+    def __exit_signal_func(self):
+        self._must_exit = True
 
     def _add_step(self, step: IPipe):
         self._pipes.append(step)
 
+    def _add_exit_check(self, check: Callable[[Any], bool]):
+        self._add_step(ExitCheckPipe(check, self.__exit_signal_func))
+
     def flow(self, data: Any):
         """Makes the pipeline flow; run its steps/pipes."""
+        self._data = data
         for pipe in self._pipes:
-            data = pipe.flow(data)
-        return data
+            self._data = pipe.flow(self._data)
+
+            if self._must_exit:
+                break
+
+        return self._data
 
     @abstractmethod
     def build_pipeline(self):
